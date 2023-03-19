cfe520de852f5dba1f5b90175a2e3bf281659e5c
webDotG <webdotg.inbox@gmail.com>
2023-03-20 00:41:54 +0300

    2.0 накидал начало рабочая версия

--- js/main.js
+++ js/main.js
@@ -1,86 +1,48 @@
-
-let photos = [];
-console.log(photos.length);
-//вариант как можно доавлять id
-let createUserId = function () {
-    id =  photos.length + 1;
-    console.log(photos);
-    return id
-};
-createUserId();
-
-let photoObj = {
-  id: createUserId(),
-  url: 'photos/{{случайное число от 1 до 25}}.jpg',
-  description: 'someDescription',
-  likes: 15,
-  comments: [
-    {
-      id: 0,
-      name: 'someName',
-      avatar: 'img/avatar-{{случайное число от 1 до 6}}.svg ',
-      message: 'Всё отлично!',
-    },
-  ]
-};
-console.log(photoObj);
-
-const createPhotoId = function () {
-  return {
-    id: createUserId() ,
-    url: '',
-    description: '',
-    likes: 0,
-    comments: [
-      {
-        id: 0,
-        name: '',
-        avatar: '',
-        message: '',
-      },
-    ]
-  };
-};
-console.log('создание обьекта user',createPhotoId());
-
-let usersArray = Array.from({length:25}, createUserId);
-console.log('массив обьектов usersArray', usersArray);
-
-/*
-let someObj = {
-  id: 1,
-  url: 'photos/{{случайное число от 1 до 25}}.jpg',
-  description: 'someDescription',
-  likes: 15,
-  comments: [
-    {
-      id: 1,
-      name: 'someName',
-      avatar: 'img/avatar-{{случайное число от 1 до 6}}.svg ',
-      message: 'Всё отлично!',
-    },
-  ]
-};
-
-
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
+function getRandomInteger(min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
+
   return Math.floor(result);
+}
+console.log('рандомное число ', getRandomInteger(1, 25));
+
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const generatePhotoId = createRandomIdFromRangeGenerator(1, 25);
+console.log('рандомное число generatePhotoId ', generatePhotoId());
+const generateUrlId = createRandomIdFromRangeGenerator(1, 25);
+console.log('рандомное число generateUrlId ', generateUrlId());
+const generateLikes = createRandomIdFromRangeGenerator(15, 200);
+console.log('рандомное число generateLikes ', generateLikes());
+const generateCommentsAvatar = getRandomInteger(1, 6);
+console.log('рандомное число generateCommentsAvatar ', generateCommentsAvatar);
+const message = ['Всё отлично!', 'Всё Не отлично!'];
+console.log('коментарии :', message);
+const photoIdObj = {
+  id: generatePhotoId(),
+  url: `photos/${generateUrlId()}.jpg`,//??? почему тут скобки ${generateUrlId()}
+  description: 'someDescription',
+  likes: generateLikes(),
+  comments : {
+    id: 0,
+    avatar: `img/avatar-${generateCommentsAvatar}.svg`,//??? а тут нет ${generateCommentsAvatar}
+  },
 };
-console.log('рандомное число', getRandomInteger(1, 25));
-
-
-//проверка обьекта
-console.log('обьект someObj', someObj);
-//проверка ключа обьекта
-console.log('значение ключа id', someObj['id']);
-//проверка массива внутри обьекта
-console.log('массива внутри someObj', someObj.comments);
-//проверка значения второго эдемента массива внутри обьекта
-console.log('значение первого элемента массива comments внутри someObj', someObj.comments[0].id);
-
-
-//функция для создания обьекта
-*/
+console.log(photoIdObj);
+console.log(photoIdObj.comments.id);

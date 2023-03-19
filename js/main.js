559f56e29055888c4b05ad303749a1ed31e294d6
webDotG <webdotg.inbox@gmail.com>
2023-03-19 19:00:54 +0300

    не рабочая идея туплю возврат к обучению

--- js/main.js
+++ js/main.js
@@ -1,44 +1,33 @@
-/*
-создать функцию для случайного числа, проверить числона повторение
 
- */
-
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+let photos = [];
+console.log(photos.length);
+//вариант как можно доавлять id
+let createUserId = function () {
+    id =  photos.length + 1;
+    console.log(photos);
+    return id
 };
-console.log('рандомное число', getRandomInteger(1, 25));
+createUserId();
 
-let someObj = {
-  id: 1 ,
+let photoObj = {
+  id: createUserId(),
   url: 'photos/{{случайное число от 1 до 25}}.jpg',
   description: 'someDescription',
   likes: 15,
   comments: [
     {
-      id: 1 ,
+      id: 0,
       name: 'someName',
       avatar: 'img/avatar-{{случайное число от 1 до 6}}.svg ',
       message: 'Всё отлично!',
     },
   ]
 };
-//проверка обьекта
-console.log('обьект someObj', someObj);
-//проверка ключа обьекта
-console.log('значение ключа id', someObj['id']);
-//проверка массива внутри обьекта
-console.log('массива внутри someObj', someObj.comments);
-//проверка значения второго эдемента массива внутри обьекта
-console.log('значение первого элемента массива comments внутри someObj', someObj.comments[0].id);
+console.log(photoObj);
 
-
-//функция для создания обьекта
-const createUserId = function () {
+const createPhotoId = function () {
   return {
-    id: 0 ,
+    id: createUserId() ,
     url: '',
     description: '',
     likes: 0,
@@ -52,7 +41,46 @@ const createUserId = function () {
     ]
   };
 };
-console.log('создание обьекта user',createUserId());
+console.log('создание обьекта user',createPhotoId());
 
 let usersArray = Array.from({length:25}, createUserId);
 console.log('массив обьектов usersArray', usersArray);
+
+/*
+let someObj = {
+  id: 1,
+  url: 'photos/{{случайное число от 1 до 25}}.jpg',
+  description: 'someDescription',
+  likes: 15,
+  comments: [
+    {
+      id: 1,
+      name: 'someName',
+      avatar: 'img/avatar-{{случайное число от 1 до 6}}.svg ',
+      message: 'Всё отлично!',
+    },
+  ]
+};
+
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+console.log('рандомное число', getRandomInteger(1, 25));
+
+
+//проверка обьекта
+console.log('обьект someObj', someObj);
+//проверка ключа обьекта
+console.log('значение ключа id', someObj['id']);
+//проверка массива внутри обьекта
+console.log('массива внутри someObj', someObj.comments);
+//проверка значения второго эдемента массива внутри обьекта
+console.log('значение первого элемента массива comments внутри someObj', someObj.comments[0].id);
+
+
+//функция для создания обьекта
+*/

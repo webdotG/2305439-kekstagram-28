4186b7a7c9ef6df3b71edb5b19ae89f9ad295c33
webDotG <webdotg.inbox@gmail.com>
2023-04-08 22:40:55 +0300

    home work need help

--- js/scale.js
+++ js/scale.js
@@ -9,10 +9,10 @@ const biggerButtonElement = document.querySelector('.scale__control--bigger');
 const scaleInputElement = document.querySelector('.scale__control--value');
 const imageElement = document.querySelector('.img-upload__preview img');
 
-//
-const scaleImage = (value) => {
-  imageElement.style.transform = `scale(${value / 100})`;//получаю нужный процент
-  scaleInputElement.value = `${value}%`;
+//функция изменения размера фото
+const scaleImage = (value) => {//принимает в сеья какое-то значение
+  imageElement.style.transform = `scale(${value / 100})`;//так как свойство стайл трансформ в css ситается от 0 до 1 то нужно получить процент от некого значения и передать его свойству css
+  scaleInputElement.value = `${value}%`;//дом элементу в значение взлью передаю передаю результат вычислений и знак процента
 };
 
 //
@@ -25,9 +25,9 @@ const onSmallerButtonClick = () => {
   scaleImage(newValue);
 };
 
-//
+//функция увелечения картинки при нажатии
 const onBiggerButtonClick = () => {
-  const currentValue = parseInt(scaleInputElement.value, 10);
+  const currentValue = parseInt(scaleInputElement.value, 10);//
   let newValue = currentValue + SCALE_STEP;
   if (newValue > MAX_SCALE) {
     newValue = MAX_SCALE;
@@ -35,13 +35,13 @@ const onBiggerButtonClick = () => {
   scaleImage(newValue);
 };
 
-//
+//функция перкдает картинке некие дефолтные парамеиры
 const resetScale = () =>scaleImage(DEFAULT_SCALE);
 
-//
+//отлавливаю событие на элементе и включаю функцию кноаки уменьшения
 smallerButtonElement.addEventListener('click', onSmallerButtonClick);
-//
+//отлавливаю событие на элементе и включаю функцию кноаки увелечения
 biggerButtonElement.addEventListener('click', onBiggerButtonClick);
 
-export {resetScale};
+export {resetScale};//экспортиру функцыию
 

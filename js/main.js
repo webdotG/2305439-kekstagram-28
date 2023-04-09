b93963389dd0df69801a9758e352a656f879a3b7
webDotG <webdotg.inbox@gmail.com>
2023-04-09 19:32:32 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 import {getPictures} from './data.js';
 import {renderThumbnails} from './thumbnail-photos.js';
-import {showFormLoadImg, hideFormLoadImg} from './validate-form.js';
 
 const pictures = getPictures();
 renderThumbnails(pictures);
-showFormLoadImg();
-hideFormLoadImg();
